d54d44ae937837741312f92bdf587d0c28178ac2
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-10-05 10:13:54 -0400

    0.0.77

--- be-intersectional.js
+++ be-intersectional.js
@@ -1,13 +1,22 @@
 // @ts-check
 import { BE } from 'be-enhanced/BE.js';
-/** @import {Actions, PAP, AllProps, AP, IntersectionalActions} from './types.d.ts' */;
+/** @import {Actions, PAP, AllProps, AP, IntersectionalActions, BAP} from './ts-refs/be-intersectional/types.d.ts' */;
 
 /**
  * @implements {IntersectionalActions}
  */
 class BeIntersectional extends BE {
+    /** @type {IntersectionObserver | undefined} */
     #observer;
+    /**
+     * @type {number | undefined}
+     */
     #echoTimeout;
+    /**
+     * 
+     * @param {BAP} self 
+     * @returns 
+     */
     onOptions(self) {
         this.disconnect();
         const { rootClosest, observeClosest, options, enhancedElement, enterDelay } = self;
@@ -18,28 +27,31 @@ class BeIntersectional extends BE {
             }
             options.root = root;
         }
+        /** @type {Element | null} */
         let targetToObserve = enhancedElement;
         if (observeClosest !== undefined) {
             targetToObserve = enhancedElement.closest(observeClosest);
         }
+        if(targetToObserve === null) throw 404;
         const observer = new IntersectionObserver((entries, observer) => {
             for (const entry of entries) {
                 const intersecting = entry.isIntersecting;
                 self.isIntersecting = intersecting;
-                clearTimeout(this.#echoTimeout);
-                this.#echoTimeout = setTimeout(() => {
+                const echoTimeout = this.#echoTimeout;
+                if(echoTimeout !== undefined) clearTimeout(echoTimeout);
+                this.#echoTimeout = /** @type {number} */(/** @type {unknown} */(setTimeout(() => {
                     try {
                         //note -- no more proxy, so maybe the try catch is overkill now
                         self.isIntersectingEcho = intersecting; //sometimes proxy is revoked
                     }
                     catch (e) { }
-                }, enterDelay);
+                }, enterDelay)));
             }
         }, options);
         setTimeout(() => {
             observer.observe(targetToObserve);
         }, enterDelay);
-        self.#observer = observer;
+        this.#observer = observer;
         return {
             resolved: true
         };
@@ -58,6 +70,10 @@ class BeIntersectional extends BE {
     // onNotIntersectingEcho(self) {
     //     self.isNotIntersectingEcho = !self.isIntersectingEcho;
     // }
+    /**
+     * 
+     * @param {Element} el 
+     */
     async detach(el) {
         this.disconnect();
     }
